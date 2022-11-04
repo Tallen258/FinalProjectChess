71b561f43f5224154a3034d6cc4a9c4c48f8dde1
Aeguez <112586458+Aeguez@users.noreply.github.com>
2022-11-03 21:30:22 -0500

    Add files via upload
    Chess Gallery

--- Game1.cs
+++ Game1.cs
@@ -1,72 +1,52 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using System;
 
-namespace FinalProjectChess;
-
-public class Game1 : Game
+namespace ChessGallery
 {
-    private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
-    private (string, int)[] gameBoard = new (string, int)[64];
-private Texture2D boardimage;
-
-    private void createBord()
+    public class Game1 : Game
     {
-        string[] letter = new string[] { "a", "b", "c", "d", "e", "f", "g", "h" };
-        int i = 0;
-        for (int x = 0; x < gameBoard.Length; x++)
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Game1()
         {
-            if (x % 8 == 0&& x!=0)
-            {
-             i++;  
-            }
-            this.gameBoard[x] = (letter[i],x%8+1);
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
         }
-    }
-    public Game1()
-    {
-        _graphics = new GraphicsDeviceManager(this);
-        Content.RootDirectory = "Content";
-        IsMouseVisible = true;
-    }
 
-    protected override void Initialize()
-    {
-        // TODO: Add your initialization logic here
-        createBord();
-        for(int i = 0; i<gameBoard.Length;i++)
-Console.WriteLine(gameBoard[i]);
-        base.Initialize();
-    }
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
 
-    protected override void LoadContent()
-    {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
-this.boardimage = Content.Load<Texture2D>("board_plain_04");
+            base.Initialize();
+        }
 
-        // TODO: use this.Content to load your game content here
-    }
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-    protected override void Update(GameTime gameTime)
-    {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
+            // TODO: use this.Content to load your game content here
+        }
 
-        // TODO: Add your update logic here
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
 
-        base.Update(gameTime);
-    }
+            // TODO: Add your update logic here
 
-    protected override void Draw(GameTime gameTime)
-    {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
- _spriteBatch.Begin();
-    _spriteBatch.Draw(boardimage, new Vector2(0, 0), Color.White);
-    _spriteBatch.End();
-        // TODO: Add your drawing code here
+            base.Update(gameTime);
+        }
 
-        base.Draw(gameTime);
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
     }
 }
